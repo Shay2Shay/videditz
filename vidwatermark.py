698e51033fdaae93b682ef458d0ee645b00a6734
Arka Mondal <arkamondalofficial@gmail.com>
2024-05-02 16:15:27 +0530

    fixed watermark

--- vidwatermark.py
+++ vidwatermark.py
@@ -1,36 +1,19 @@
-from pathlib import Path
-from inspect import cleandoc
-from concurrent.futures import ProcessPoolExecutor
-from moviepy.editor import VideoFileClip, CompositeVideoClip, ImageClip
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 
-class Watermark:
-    def __init__(self, files, f_output, v_position, h_position, f_measure, f_fps, f_overwrite):
-        self.files = files
-        self.f_output = f_output
-        self.v_position = v_position
-        self.h_position = h_position
-        self.f_measure = f_measure
-        self.f_fps = f_fps
-        self.f_overwrite = f_overwrite
-        self.resize = {'small': 30, 'medium': 60, 'large': 90}
+def add_watermark(video_path, watermark_text, output_path):
+    video_clip = VideoFileClip(video_path)
 
-    def process_watermark(self, file):
-        video = VideoFileClip(str(file))
-        watermark = ImageClip('assets/watermark.png').set_duration(video.duration).resize(
-            height=self.resize[self.f_measure]).margin(right=8, top=8, left=8, bottom=8, opacity=0).set_pos((self.h_position, self.v_position))
+    watermark = (TextClip(watermark_text, fontsize=40, color='white', font='Arial-Bold')
+                 .set_position(('right', 'bottom'))
+                 .set_duration(video_clip.duration))
 
-        new_filename = f'watermarked_{self.v_position}_{self.h_position}_{file.name}'
+    video_with_watermark = CompositeVideoClip([video_clip, watermark])
 
-        final_folder = self.f_output.joinpath(file.stem)
-        Path(final_folder).mkdir(parents=True, exist_ok=True)
+    video_with_watermark.write_videofile(output_path, codec='libx264', audio_codec='aac')
 
-        final_output = final_folder.joinpath(new_filename)
-        final_file = CompositeVideoClip([video, watermark])
-        final_file.write_videofile(
-            str(final_output), fps=self.f_fps if self.f_fps else video.fps, temp_audiofile=Path(final_folder.joinpath(f'{file.stem}_TEMP_FILE.mp3')))
+if __name__ == "__main__":
+    video_path = "video1-audio.mp4"
+    watermark_text = "MS Project"
+    output_path = f"{video_path[:-4]}_with_watermark.mp4"
 
-        video.close()
-
-    def watermark_processor(self):
-        with ProcessPoolExecutor() as executor:
-            executor.map(self.process_watermark, self.files)
+    add_watermark(video_path, watermark_text, output_path)
