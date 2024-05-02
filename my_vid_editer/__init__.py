2be85a97254c6f3ec0746cf0051907b971100523
Shay2Shay <batterytedd@gmail.com>
2024-05-02 15:41:08 +0530

    Almost done:

--- my_vid_editer/__init__.py
+++ my_vid_editer/__init__.py
@@ -3,6 +3,7 @@ from concat_video import concat_video
 from crop_video import crop_video
 from vid_plus_audio import add_audio_to_video
 from watermark import Watermark
+from vid_to_audio import vid_to_audio
 
 import os
 
@@ -18,11 +19,16 @@ if __name__ == "__main__":
     )
     obj.convert()
 
-    print("===================Testing Concat Video===================")
+    print("===================Testing Audio from Video===================")
+    vid_to_audio()
 
     print("===================Testing Concat Video===================")
+    obj = concat_video()
+    obj.concat()
 
     print("===================Testing Crop Video===================")
+    obj = crop_video()
+    obj.crop()
 
     print("===================Testing Add audio to Video===================")
 
