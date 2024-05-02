9909ea0b4108138806b2e92a2e68086f533c0235
Shay2Shay <batterytedd@gmail.com>
2024-05-02 15:28:15 +0530

    bug fix

--- my_vid_editer/crop_video.py
+++ my_vid_editer/crop_video.py
@@ -5,7 +5,7 @@ class crop_video:
     def __init__(self, video="video2.mp4") -> None:
         self.video = VideoFileClip(video)
 
-    def crop(self, output_path="cropped_video.mp4", x1=100,y1=100,x2=400,x3=300):
+    def crop(self, output_path="cropped_video.mp4", x1=100,y1=100,x2=400,y2=300):
         video = self.video
         cropped_video = video.crop(x1=x1, y1=y1, x2=x2, y2=y2)
         cropped_video.write_videofile(output_path)
